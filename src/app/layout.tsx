65e703ba4252eb7343c52b2fa1774f8ca4eadeb3
yuvraj141 <yuvrajhossain141@gmail.com>
2025-07-07 19:32:00 +0600

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <h1 className="text-3xl">hello bhai</h1>
         {children}
       </body>
     </html>
